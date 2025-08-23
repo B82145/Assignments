e1596babb4f3614e72dc2ce3a35ada37954f39ef
B82145 <balkrishnajha201@gmail.com>
2025-08-23 15:02:33 +0530

    Assignment- 7

---
+++ AnagramGroup.py
@@ -0,0 +1,15 @@
+
+ words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+result = {"eat": [], "tan": [], "bat": []}
+
+# Create a map of sorted keys for result dictionary
+key_map = {key: ''.join(sorted(key)) for key in result}
+
+for word in words:
+    sorted_word = ''.join(sorted(word))
+    for key, sorted_key in key_map.items():
+        if sorted_word == sorted_key:
+            result[key].append(word)
+            break
+
+print(result)
